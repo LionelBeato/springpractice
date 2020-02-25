5e4e8e331f1d49593c7bf56046e2fe1247afa03a
LionelBeato <lionel.beato@gmail.com>
2020-02-25 14:59:12 -0500

    adds comments and static resources

--- src/main/java/com/lionel/springpractice/Controller.java
+++ src/main/java/com/lionel/springpractice/Controller.java
@@ -4,10 +4,11 @@ package com.lionel.springpractice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// controller class with annotation, @RestController is a more fleshed out version of @Controller
 @RestController
 public class Controller {
 
-
+    // exposing the endpoint "/hello" that will have the value of the return value of the hello() function
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World! My app works!";
--- src/main/java/com/lionel/springpractice/SpringpracticeApplication.java
+++ src/main/java/com/lionel/springpractice/SpringpracticeApplication.java
@@ -3,6 +3,8 @@ package com.lionel.springpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// main app class with main method serving as my entrypoint
+// note the annotation telling spring boot this is the entry point
 @SpringBootApplication
 public class SpringpracticeApplication {
 
